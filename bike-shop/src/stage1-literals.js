32c57dd2aac6fb82cc867bc589659fb40283d949
erikasf <erika@mendux.com>
2017-04-17 17:36:44 -0700

    bike passess

--- bike-shop/src/stage1-literals.js
+++ bike-shop/src/stage1-literals.js
@@ -1,5 +1,28 @@
+"use strict";
+
 const myBike = {
   // your code here
-}
+    name: "Roadster",
+    price: 199.99,
+    frame:{
 
-module.exports = myBike
+      height: 55,
+      color: 'blue',
+      style:'cruiser'
+    },
+    brakes:{
+      back:true,
+      front: false
+    },
+    tires:{
+      diameter: 22,
+      type: "fat"
+    },
+    rings:[2,5]
+
+
+
+
+};
+
+module.exports = myBike;
