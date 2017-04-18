26587e401542cd25f48ac9f04bf2dcc9a8e11bf3
erikasf <erika@mendux.com>
2017-04-18 16:15:47 -0700

    stage2

--- bike-shop/src/stage2-constructors.js
+++ bike-shop/src/stage2-constructors.js
@@ -1,17 +1,32 @@
-function Bike() {
-  // your code here
+function Bike(name, price) {
+  this.name = name,
+  this.price = price,
+  this.tires = [new Tire(),new Tire()],
+  this.frame  = new Frame(),
+  this.rings = [3,7],
+  this.brakes = {
+    front: true,
+    back: true
+  }
 }
 
-function Frame() {
-  // your code here
+
+function Frame(color="black",size=55,style = "street") {
+  this.color = color,
+  this.size = size,
+  this.style = style
+
+
 }
 
-function Tire() {
+function Tire(diameter = 22,type ="street") {
   // your code here
+  this.diameter = diameter,
+  this.type = type
 }
 
 module.exports = {
   Bike: Bike,
   Frame: Frame,
   Tire: Tire
-}
+};
