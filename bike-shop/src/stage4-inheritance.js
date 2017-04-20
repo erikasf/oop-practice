994332a520fbf65804834c72551c57a56b9ebe3f
erikasf <erika@mendux.com>
2017-04-20 04:27:39 -0700

    not all of it is workign

--- bike-shop/src/stage4-inheritance.js
+++ bike-shop/src/stage4-inheritance.js
@@ -6,41 +6,103 @@ class Bike {
     // your code here
     //
 
-    constructor(frame, tire, shocks) {
-        this.frame = frame;
-        this.tire = tire;
-        this.shocks = shocks;
-
-    }
-    super(frame, tire,shocks);
-
-}
-class lunch extends Bike {
-    constructor(frame,tire,shocks,){
-
-    }
-
     class Frame {
-        consructor(){
+  constructor(color='black', size=55, style='street') {
+    this.color = color
+    this.size = size
+    this.style = style
+  }
+}
 
-        };
-// your code here
+class Tire {
+  constructor(diameter=22, type='street') {
+    this.diameter = diameter
+    this.type = type
+    this._flat = false
+  }
+
+  isFlat() { return this._flat }
+  puncture() { this._flat = true }
+  repair() { this._flat = false }
+}
+
+class DirtTire extends Tire {
+  constructor(diameter) {
+    super(diameter, 'dirt')
+  }
+}
+
+class RoadTire extends Tire {
+  constructor(diameter) {
+    super(diameter, 'road')
+  }
+}
+
+class Bike {
+  constructor(name, price) {
+    const DEFAULT_RINGS = [3, 7]
+    const DEFAULT_BRAKES = { front: true, back: true }
+
+    this.name = name
+    this.price = price
+    this.frame = new Frame()
+    this.tires = [new Tire(), new Tire()]
+    this.rings = defaultRings
+    this.brakes = defaultBrakes
+    this.moving = false
+  }
+
+  gearSpeeds() { return this.rings[0] * this.rings[1] }
+
+  pedal() {
+    if (this.tires[0].isFlat() || this.tires[1].isFlat()) {
+      throw Error("Can't pedal with a flat tire")
     }
 
-    class Tire {
-        myTire['default'] = 20;
-    }//default tires are 20"inches so smaller
+    this.moving = true
+  }
 
+  brake() { this.moving = false }
+  isMoving() { return this.moving }
+}
 
-      
+class MountainBike extends Bike {
+      //using the spread operation so that we can keep adding arguments.
+}
+  constructor(...args) {
+    super(...args)
 
-  
-  //has default gearSpeeds 
-    }
-//method for class Bike with adjustsShocks(mountainBike) adjust
+    this.frame.style = 'mountain'
+    this.tires = [new DirtTire(), new DirtTire()]
+    this._shocks = 20
+  }
+
+  get shocks() { return this._shocks }
+  adjustShocks(newSetting) { this._shocks = newSetting }
+}
+
+class BMXBike extends Bike {
+  constructor(...args) {
+    super(...args)
+
+    this.brakes = { front: false, back: true }
+    this.tires = [new Tire(20), new Tire(20)]
+  }
+}
+
+class RacingBike extends Bike {
+  constructor(...args) {
+    super(...args)
+
+    this.frame.style = 'racing'
+    this.tires = [new RoadTire(), new RoadTire()]
+    this.rings = [3, 10]
+  }
+}
 
 module.exports = {
   Bike: Bike,
-d
-  // you'll need to export new classes here
-};
+  MountainBike: MountainBike,
+  BMXBike: BMXBike,
+  RacingBike: RacingBike
+}
