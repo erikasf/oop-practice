53337051b33e37ed13c173dd5a082f8867735125
erikasf <erika@mendux.com>
2017-04-21 12:15:47 -0700

    adding all classes done in a way that is best guess at how to format , did not do the individual files don't know what should extend and be required in what

--- bike-shop/src/stage4-inheritance.js
+++ bike-shop/src/stage4-inheritance.js
@@ -1,5 +1,5 @@
 "use strict";
-class Bike {
+
     //expects mountainbike.prototype to be instance of Bike
     //default params = frame = mountain bike and tires = dirt and the shokes  are default at 20 for defaultSagSettings
     //default smaller (20") tires
@@ -7,78 +7,108 @@ class Bike {
     //
 
     class Frame {
-  constructor(color='black', size=55, style='street') {
-    this.color = color
-    this.size = size
-    this.style = style
-  }
+    constructor(color = 'black', size = 55, style = 'street') {
+        this.color = color
+        this.size = size
+        this.style = style
+    }
 }
 
 class Tire {
-  constructor(diameter=22, type='street') {
-    this.diameter = diameter
-    this.type = type
-    this._flat = false
-  }
+    constructor(diameter = 22, type = 'street') {
+        this.diameter = diameter
+        this.type = type
+        this._flat = false
+    }
 
-  isFlat() { return this._flat }
-  puncture() { this._flat = true }
-  repair() { this._flat = false }
+    isFlat() {
+        return this._flat
+    }
+
+    puncture() {
+        this._flat = true
+    }
+
+    repair() {
+        this._flat = false
+    }
 }
 
 class DirtTire extends Tire {
-  constructor(diameter) {
-    super(diameter, 'dirt')
-  }
+    constructor(diameter) {
+        super(diameter, 'dirt')
+    }
 }
 
 class RoadTire extends Tire {
-  constructor(diameter) {
-    super(diameter, 'road')
-  }
+    constructor(diameter) {
+        super(diameter, 'road')
+    }
 }
 
 class Bike {
-  constructor(name, price) {
-    const DEFAULT_RINGS = [3, 7]
-    const DEFAULT_BRAKES = { front: true, back: true }
+    constructor(name, price) {
+        const defaultRings = [3, 7]
+        const defaultBrakes = {front: true, back: true}
 
-    this.name = name
-    this.price = price
-    this.frame = new Frame()
-    this.tires = [new Tire(), new Tire()]
-    this.rings = defaultRings
-    this.brakes = defaultBrakes
-    this.moving = false
-  }
-
-  gearSpeeds() { return this.rings[0] * this.rings[1] }
-
-  pedal() {
-    if (this.tires[0].isFlat() || this.tires[1].isFlat()) {
-      throw Error("Can't pedal with a flat tire")
+        this.name = name
+        this.price = price
+        this.frame = new Frame()
+        this.tires = [new Tire(), new Tire()]
+        this.rings = defaultRings
+        this.brakes = defaultBrakes
+        this.moving = false
     }
 
-    this.moving = true
-  }
+    gearSpeeds() {
+        return this.rings[0] * this.rings[1]
+    }
 
-  brake() { this.moving = false }
-  isMoving() { return this.moving }
+    pedal() {
+        if (this.tires[0].isFlat() || this.tires[1].isFlat()) {
+            throw Error("Can't pedal with a flat tire")
+        }
+
+        this.moving = true
+    }
+
+    brake() {
+        this.moving = false
+    }
+
+    isMoving() {
+        return this.moving
+    }
 }
 
 class MountainBike extends Bike {
-      //using the spread operation so that we can keep adding arguments.
+    //using the spread operation so that we can keep adding arguments.
+
+constructor(...args)
+{
+    super(var shocks = 20,frame,tires = "dirt");
+
+
+
+        shocks = shocks.
+        frame.style = 'mountain',
+        this.tires = [new DirtTire(), new DirtTire()],
+        this.shocks = shocks;
+
+
+
+    MountainBike.adjustShocks();
 }
-  constructor(...args) {
-    super(...args)
 
-    this.frame.style = 'mountain'
-    this.tires = [new DirtTire(), new DirtTire()]
-    this._shocks = 20
-  }
 
-  get shocks() { return this._shocks }
-  adjustShocks(newSetting) { this._shocks = newSetting }
+function(newSetting){
+  this.shocks(20);
+
+}
+adjustShocks(newSetting)
+    {
+    this._shocks = newSetting();
+    }
 }
 
 class BMXBike extends Bike {
@@ -100,9 +130,9 @@ class RacingBike extends Bike {
   }
 }
 
-module.exports = {
-  Bike: Bike,
-  MountainBike: MountainBike,
-  BMXBike: BMXBike,
-  RacingBike: RacingBike
+    module.exports = {
+      Bike: Bike,
+      MountainBike: MountainBike,
+      BMXBike: BMXBike,
+      RacingBike: RacingBike
 }
